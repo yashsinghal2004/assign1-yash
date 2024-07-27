b0cd0872119b8830ee2eb08a96e38cd9694d78ee
22103251 <22103251@pdc.jiit>
2024-07-27 10:16:05 +0530

    1st assignment

---
+++ q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+int arr[n];
+int result=0;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+for(int i=0;i<n;i++){
+    result=result+arr[i];
+}
+int avg=result/n;
+cout<<"Average is "<<avg<<endl;
+return 0;
+}
